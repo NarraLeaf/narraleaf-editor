bc66717610d54ad64a25e8c017c99fe8a04f027f
York <helloyork@icloud.com>
2024-12-14 10:37:56 -0800

    update: I won't use npm again

--- src/lib/components/Editor/CharacterBrowser/CharacterBrowser.tsx
+++ src/lib/components/Editor/CharacterBrowser/CharacterBrowser.tsx
@@ -1,6 +1,7 @@
 import {useEditor} from "@lib/providers/Editor";
-import {VerticalBox} from "@lib/utils/components";
+import {HorizontalBox, VerticalBox} from "@lib/utils/components";
 import {CharacterBrowserFolder} from "@lib/components/Editor/CharacterBrowser/CharacterBrowserFolder";
+import {FolderPlusIcon} from "@heroicons/react/24/outline";
 
 export function CharacterBrowser() {
     const editor = useEditor();
@@ -12,6 +13,18 @@ export function CharacterBrowser() {
             <VerticalBox
                 className={"h-full w-full overflow-y-scroll"}
             >
+                <HorizontalBox
+                    className={"w-full h-6 place-content-between items-center"}
+                >
+                    <span className={"text-nowrap select-none p-2"}>
+                        Characters
+                    </span>
+                    <div
+                        className={"text-nowrap cursor-pointer hover:bg-gray-200 p-2"}
+                    >
+                        <FolderPlusIcon className={"h-4 w-4"}/>
+                    </div>
+                </HorizontalBox>
                 {characterGroups.map(([name, group], index) => (
                     <CharacterBrowserFolder name={name} group={group} key={index}/>
                 ))}
